b1522759850c25a9480cacc69fcde8b64f1b3e9e
Paul Cuellar <pacuellarp@unal.edu.co>
2023-09-13 20:22:11 -0500

    index.js models done

--- db/models/index.js
+++ db/models/index.js
@@ -1,23 +1,36 @@
-const { User, UserSchema } = require('./user.model');
-const { Customer, CustomerSchema } = require('./customer.model');
+const { Gender, GenderSchema } = require('./gender.model');
+const { Size, SizeSchema } = require('./size.model');
+const { Color, ColorSchema } = require('./color.model');
 const { Category, CategorySchema } = require('./category.model');
+const { SizeProduct, SizeProductSchema } = require('./size-product.model');
+const { ColorProduct, ColorProductSchema } = require('./color-product.model')
 const { Product, ProductSchema } = require('./product.model');
-const { Order, OrderSchema } = require('./order.model');
-const { OrderProduct, OrderProductSchema } = require('./order-product.model');
+const { Photo, PhotoSchema } = require('./photo.model');
+const { Video, VideoSchema } = require('./video.model');  
+
 
 function setupModels(sequelize) {
-  User.init(UserSchema, User.config(sequelize));
-  Customer.init(CustomerSchema, Customer.config(sequelize));
-  Category.init(CategorySchema, Category.config(sequelize));
-  Product.init(ProductSchema, Product.config(sequelize));
-  Order.init(OrderSchema, Order.config(sequelize));
-  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
-  User.associate(sequelize.models);
-  Customer.associate(sequelize.models);
+  Gender.init(GenderSchema, Gender.config(sequelize));
+  Size.init(SizeSchema, Size.config(sequelize));
+  Color.init(ColorSchema, Color.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize));
+  SizeProduct.init(SizeProductSchema, SizeProduct.config(sequelize));
+  ColorProduct.init(ColorProductSchema, ColorProduct.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
+  Photo.init(PhotoSchema, Photo.config(sequelize));
+  Video.init(VideoSchema, Video.config(sequelize));
+
+
+  Gender.associate(sequelize.models);
+  Size.associate(sequelize.models);
+  Color.associate(sequelize.models);
   Category.associate(sequelize.models);
+  SizeProduct.associate(sequelize.models);
+  ColorProduct.associate(sequelize.models);
   Product.associate(sequelize.models);
-  Order.associate(sequelize.models);
+  Photo.associate(sequelize.models);
+  Video.associate(sequelize.models);
 }
 
 module.exports = setupModels;
