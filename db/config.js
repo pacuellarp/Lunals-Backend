87bbaa744b1462cd31289b42630c2b0fadab9d4b
Paul Cuellar <pacuellarp@unal.edu.co>
2023-10-26 16:51:35 -0500

    env

--- db/config.js
+++ db/config.js
@@ -4,7 +4,7 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 
 let URI = '';
-if (!(config.env == 'development')) {
+if (config.env == 'development') {
   URI = `${config.dialect}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 } else {
   URI = config.dbUrl;
