b2f5937612b23b1ece009d6d6bd090dbbf8aa138
Paul Cuellar <pacuellarp@unal.edu.co>
2023-11-17 05:43:11 -0500

    Video patch service fixed

--- services/video.service.js
+++ services/video.service.js
@@ -1,61 +1,57 @@
 const boom = require('@hapi/boom');
 
-const { models }= require('./../libs/sequelize');
+const { models } = require('./../libs/sequelize');
 
 class VideoService {
-
-    constructor(){
-    }
-    async create(data) {
-      const newVideo = await models.Video.create(data);
-      return newVideo;
-    }
-  
-    async find() {
-      const videos = await models.Video.findAll();
-      return videos;
-    }
-  
-    async findOne(id) {
-      const video = await models.Video.findByPk(id, {
-        include: ['product']
-      });
-      return video;
-    }
-  
-    async update(id, changes) {
-      try {
-        const size = await models.Size.findByPk(id);
-    
-        if (!size) {
-          throw boom.notFound('Size not found');
-        }
-    
-        await size.update(changes);
-    
-        return size;
-      } catch (error) {
-        throw boom.badImplementation('Error updating size', error);
-      }
-    }
-    
-    async delete(id) {
-      try {
-        const size = await models.Size.findByPk(id);
-    
-        if (!size) {
-          throw boom.notFound('Size not found');
-        }
-    
-        await size.destroy();
-    
-        return { message: 'Size deleted successfully' };
-      } catch (error) {
-        throw boom.badImplementation('Error deleting size', error);
-      }
-    }
-    
-  
+  constructor() {}
+  async create(data) {
+    const newVideo = await models.Video.create(data);
+    return newVideo;
   }
-  
-  module.exports = VideoService;
+
+  async find() {
+    const videos = await models.Video.findAll();
+    return videos;
+  }
+
+  async findOne(id) {
+    const video = await models.Video.findByPk(id, {
+      include: ['product'],
+    });
+    return video;
+  }
+
+  async update(id, changes) {
+    try {
+      const video = await models.Video.findByPk(id);
+
+      if (!video) {
+        throw boom.notFound('Video not found');
+      }
+
+      await video.update(changes);
+
+      return video;
+    } catch (error) {
+      throw boom.badImplementation('Error updating video', error);
+    }
+  }
+
+  async delete(id) {
+    try {
+      const video = await models.Video.findByPk(id);
+
+      if (!video) {
+        throw boom.notFound('Video not found');
+      }
+
+      await video.destroy();
+
+      return { message: 'Video deleted successfully' };
+    } catch (error) {
+      throw boom.badImplementation('Error deleting video', error);
+    }
+  }
+}
+
+module.exports = VideoService;
